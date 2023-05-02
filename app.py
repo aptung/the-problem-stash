8fee3d3fb01e4db725138385c9f2fa2fc24c9268
aptung <andrew.tung@menloschool.org>
2023-05-02 14:48:35 -0700

    added a "recently solved" page

--- app.py
+++ app.py
@@ -7,35 +7,26 @@ app = Flask(__name__)
 
 app.secret_key = "sdfjowiefjw98r32jfhregu8392"
 
-# TODO: Fill in methods and routes
 
 @app.route("/")
 def home():
     # testproblem=Problem("Test problem", "here is some content for the test problem", "42", "algebra")
-    # testuser=User("test1", "password1")
-    
-    # db_session.add(testproblem)
-    # db_session.add(testuser)
-    # db_session.commit()
-    # testproblem.solved_by.append(testuser)
-    # db_session.commit()
-    
     # testproblem2=Problem("Test problem2", "new content", "21", "geometry")
-    # db_session.add(testproblem2)
-    # db_session.commit()
-
     # testproblem3=Problem("Test problem3", "sdssdfsdsd", "1", "algebra")
     # testproblem4=Problem("Test problem4", "sdssdsdsdfsdffsdsd", "3", "number theory")
     # testproblem5=Problem("Test problem5", "sdssdefeefeffeefeffsdsd", "7", "algebra")
-
+    # testproblem6=Problem("Test problem6", "sdfwefijwgerwwee", "17", "algebra")
+    # testuser=User("test1", "password1")
+    
+    # db_session.add(testproblem)
+    # db_session.add(testproblem2)
     # db_session.add(testproblem3)
     # db_session.add(testproblem4)
     # db_session.add(testproblem5)
+    # db_session.add(testproblem6)
+    # db_session.add(testuser)
     # db_session.commit()
 
-    # testproblem6=Problem("Test problem6", "sdfwefijwgerwwee", "17", "algebra")
-    # db_session.add(testproblem6)
-    # db_session.commit()
     
     problems = random.sample(list(db_session.query(Problem)), 6)
     logged_in = "username" in session
@@ -43,6 +34,8 @@ def home():
 
 @app.route("/login", methods=("GET", "POST"))
 def login():
+    if "username" in session:
+        return redirect(url_for('solve'))
     if request.method=="GET":
         return render_template("login.html")
     if request.method=="POST":
@@ -60,6 +53,8 @@ def login():
 
 @app.route("/signup", methods=("GET", "POST"))
 def signup():
+    if "username" in session:
+        return redirect(url_for('solve'))
     if request.method=="GET":
         return render_template("signup.html")
     if request.method=="POST":
@@ -69,10 +64,10 @@ def signup():
         user = db_session.query(User).where(User.username==username).first()
         if user is not None:
             flash("That username is already taken", "error")
-            return render_template("signup.html")
+            return render_template("signup.html", logged_in=False)
         elif password != password2:
             flash("Those passwords don't match", "error")
-            return render_template("signup.html")
+            return render_template("signup.html", logged_in=False)
         else:
             new_user=User(username, password)
             db_session.add(new_user)
@@ -93,7 +88,8 @@ def logout():
 
 @app.route("/solve", methods=("GET", "POST"))
 def solve():
-    logged_in = "username" in session
+    if "username" not in session:
+        return redirect(url_for('login'))
     if request.method == "GET":
         problems_solved = db_session.query(User).where(User.username == session["username"]).first().problems_solved
         problems_unsolved = []
@@ -105,7 +101,7 @@ def solve():
             return redirect(url_for('home'))
         rand_problem = random.choice(problems_unsolved)
         session["title"] = rand_problem.title
-        return render_template("solve.html", problem=rand_problem, logged_in=logged_in)
+        return render_template("solve.html", problem=rand_problem, logged_in=True)
     if request.method=="POST":
         answer=request.form["answer"]
         current_problem = db_session.query(Problem).where(Problem.title==session["title"]).first()
@@ -117,7 +113,16 @@ def solve():
             return redirect(url_for('solve'))
         else:
             flash("That's incorrect", "info")
-            return render_template("solve.html", problem=current_problem, logged_in=logged_in)
+            return render_template("solve.html", problem=current_problem, logged_in=True)
+
+# this assumes we are already logged in
+@app.route("/recentlysolved")
+def recentlysolved():
+    if "username" not in session:
+        return redirect(url_for('login'))
+    # .all() ensures that a list is returned instead of an iterator
+    problems_dates = db_session.query(Problem, Solve.time_solved).where(Problem.title==Solve.problem_id).join(Solve, Solve.user_id==session["username"]).group_by(Problem.title).order_by(Solve.time_solved).limit(10).all()
+    return render_template("recentlysolved.html", problems_dates = problems_dates, logged_in=True)
 
 @app.before_first_request
 def setup():
